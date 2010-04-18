1f034944663c59da9efc4a93711b5bf71283eba6
The Stig <the.git.dev@gmail.com>
2010-04-18 00:39:36 -0400

    Implemented Dimmer functionality

--- Classes/DimmerAppDelegate.m
+++ Classes/DimmerAppDelegate.m
@@ -3,19 +3,48 @@
 //  Dimmer
 //
 //  Created by The Stig on 10-04-17.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Copyright The Stig 2010. All rights reserved.
 //
 
 #import "DimmerAppDelegate.h"
+
+@interface UIApplication(private)
+
+- (void) setBrightnessLevel: (float) brightness;
+- (float) brightnessLevel;
+
+@end
+
 
 @implementation DimmerAppDelegate
 
 @synthesize window;
 
 - (void) applicationDidFinishLaunching: (UIApplication*) application
-{    
-	// Override point for customization after application launch
-	[window makeKeyAndVisible];
+{
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	UIApplication *app = [UIApplication sharedApplication];
+	float curBrightness = 0.0;
+	
+	if ([app respondsToSelector: @selector(brightnessLevel)])
+		curBrightness = [app brightnessLevel];
+
+	if (curBrightness > 0.0)
+	{
+		[defaults setFloat: curBrightness forKey: @"brightness"];
+		if ([app respondsToSelector: @selector(setBrightnessLevel:)])
+			[app setBrightnessLevel: 0.0];
+	}
+	else
+	{
+		float savedBrightness = [defaults floatForKey: @"brightness"];
+		if (savedBrightness > 0.0)
+			if ([app respondsToSelector: @selector(setBrightnessLevel:)])
+				[app setBrightnessLevel: savedBrightness];
+	}
+
+	exit(0);
+	//[window makeKeyAndVisible];
 }
 
 - (void) dealloc
